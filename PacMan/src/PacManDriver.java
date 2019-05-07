0b53e63d01e5dd2355a2eb8da021b489b4003fa4
Rudy Khanwale <chaos.rudy@gmail.com>
2019-05-07 10:14:12 -0700

    Initial Commit

---
+++ PacMan/src/PacManDriver.java
@@ -0,0 +1,226 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+public class PacManDriver extends JPanel implements ActionListener, KeyListener{
+	JFrame f = new JFrame(); 
+
+	Timer t = null;
+	boolean setupPaint = true;
+
+	static Piece[][] mapArr = null; // stores map data
+
+	boolean shiftDown = true; // determines direction of particle movement
+
+	int shiftDistance = -1; // determines amount of particle shift
+
+	int pacManX = 404;
+	int pacManXCenter = pacManX + 16;
+
+	int pacManY = 404;
+	int pacManYCenter = pacManY + 16;
+	int pacManImg = 0; 
+	double pacManDir = 0; 
+
+	public PacManDriver(){
+		scan(); // calls scan method to parse input file and initialize map
+
+		// JFrame data
+		f.setVisible(true);
+		f.setTitle("Pac-Man");
+		f.setSize(mapArr.length* 40, mapArr[0].length*40 + 100 ); // dependent on map size
+		f.setBackground(Color.BLACK);
+		f.setResizable(false);
+		f.addKeyListener(this);
+		f.add(this);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		// Timer for update
+		t = new Timer(175, this);
+		t.start();
+	}
+	public void paint(Graphics g) {
+		
+		for(int i = 0; i < mapArr.length; i++) {
+			for(int j = 0; j < mapArr[0].length; j++) {
+				mapArr[i][j].setX(40*j);
+				mapArr[i][j].setY(40*i + 80);
+				g.drawImage(mapArr[i][j].getImage(), mapArr[i][j].getX() , mapArr[i][j].getY(), this);
+				if(mapArr[i][j].hasFood()) {
+					g.drawImage(mapArr[i][j].getFoodImage(), mapArr[i][j].getX() + (40- mapArr[i][j].getFoodImage().getWidth(this))/2, mapArr[i][j].getY() + (40- mapArr[i][j].getFoodImage().getHeight(this))/2 + shiftDistance, this);
+
+				}
+			}
+		}
+
+		// Rectangle at top for score, name etc.
+		if(setupPaint) {
+		g.fillRect(0, 0, 40*mapArr.length, 80);
+
+		// User Information Display
+
+		Font f1 = new Font("Courier", 0, 30);
+		g.setColor(Color.WHITE);
+		g.setFont(f1);
+		g.drawString("Name: " + "Pickles", 50, 50);
+		g.drawString("Score: 100", 400, 50);
+		}
+
+		// Map is a 2D array of images. Draws the correct image based on input file
+
+		
+
+		//Lives
+		for(int i = 0; i < 3; i++) {
+			Image heart = Toolkit.getDefaultToolkit().getImage("PacManImageh.png");
+			g.drawImage(heart, 640 + 50*i , 15 , this);
+		}
+		Image pacMan =  Toolkit.getDefaultToolkit().getImage("Character" + Integer.toString(pacManImg) + ".png");
+		
+		g.drawImage(pacMan, pacManX, pacManY, this);
+
+	}
+	public static void scan() {
+		File file = new File("Map1.txt");
+		Scanner s;
+		try {
+			s = new Scanner(file);
+			int rows = s.nextInt(); 
+			int cols = s.nextInt();
+			mapArr = new Piece[rows][cols];
+			int iterator = 0;
+			while(s.hasNextLine() && iterator != rows) {
+
+				for(int i = 0; i < cols; i++) {
+					if(s.hasNextInt()) {
+						Piece p = new Piece(0, 0, Integer.toString(s.nextInt()));
+						mapArr[iterator][i] = p;
+
+
+					}else {
+						Piece p = new Piece(0, 0, s.next());
+						mapArr[iterator][i] = p;
+
+
+					}
+				}
+				iterator++;
+
+			}
+			iterator = 0;
+			while(s.hasNextLine() && iterator != rows) {
+
+
+				for(int i = 0; i < cols; i++) {
+					if(s.hasNextInt()) {
+						mapArr[iterator][i].setFood(false);
+						s.nextInt();
+
+					}else {
+						mapArr[iterator][i].setFood(true);
+						mapArr[iterator][i].setFoodType(s.next());
+
+
+					}
+				}
+				iterator++;
+			}
+			s.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+	public void update() {
+		if(shiftDown) {
+			shiftDistance++;
+		}else {
+			shiftDistance--;
+		}
+
+		if(shiftDistance == -1) {
+			shiftDown = true;
+		}
+		if(shiftDistance == 1) {
+			shiftDown = false;
+		}
+		if(pacManImg == 0) {
+			pacManImg = 1;
+		}else {
+			pacManImg = 0;
+		}
+	}
+	public void pacUpdate() {
+		repaint();
+	}
+	public static void main(String[] args) {
+		PacManDriver h = new PacManDriver();
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+
+
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() == 38) {
+			pacManY-= 10;
+			pacUpdate();
+		}else if(e.getKeyCode() == 39) {
+			pacManX+= 10;
+			pacUpdate();
+		}else if(e.getKeyCode() == 40) {
+			pacManY+= 10;
+			pacUpdate();
+
+		}else if(e.getKeyCode() == 37) {
+			pacManX-=10;
+			pacUpdate();
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+		repaint();
+		update();
+
+
+	}
+	public Piece wheresPacMan() {
+		int row = (pacManYCenter/40);
+		int col = (pacManXCenter/40);
+		return null; 
+		
+	}
+}
---
+++ PacMan/src/Piece.java
@@ -0,0 +1,147 @@
+import java.awt.Image;
+import java.awt.Toolkit;
+
+public class Piece {
+
+	private boolean topCol, botCol, leftCol, rightCol; // true if blocked
+	private int x, y;
+	private String type;
+	private Image img;
+	private Image foodImg;
+	private boolean food;
+	private String foodType;
+	public Piece(int x, int y, String type) {
+		this.x = x;
+		this.y = y;
+		this.type = type;
+		setImageData();
+	}
+
+	public void setImageData() {
+		setImage(type);
+		if(type.equals("1") ||
+				type.equals("7") ||
+				type.equals("8") ||
+				type.equals("b") ||
+				type.equals("c") ||
+				type.equals("d") ||
+				type.equals("f")) {
+			leftCol = true;
+		}
+
+		if(type.equals("1") ||
+				type.equals("5") ||
+				type.equals("9") ||
+				type.equals("a") ||
+				type.equals("d") ||
+				type.equals("e") ||
+				type.equals("f")) {
+			rightCol = true;
+		}
+		if(type.equals("2") ||
+				type.equals("4") ||
+				type.equals("8") ||
+				type.equals("9") ||
+				type.equals("c") ||
+				type.equals("d") ||
+				type.equals("e")) {
+			topCol = true;
+		}
+		if(type.equals("2") ||
+				type.equals("6") ||
+				type.equals("a") ||
+				type.equals("b") ||
+				type.equals("c") ||
+				type.equals("e") ||
+				type.equals("f")) {
+			botCol = true;
+		}
+
+	}
+
+	public boolean isTopCol() {
+		return topCol;
+	}
+
+	public void setTopCol(boolean topCol) {
+		this.topCol = topCol;
+	}
+
+	public boolean isBotCol() {
+		return botCol;
+	}
+
+	public void setBotCol(boolean botCol) {
+		this.botCol = botCol;
+	}
+
+	public boolean isLeftCol() {
+		return leftCol;
+	}
+
+	public void setLeftCol(boolean leftCol) {
+		this.leftCol = leftCol;
+	}
+
+	public boolean isRightCol() {
+		return rightCol;
+	}
+
+	public void setRightCol(boolean rightCol) {
+		this.rightCol = rightCol;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Image getImage() {
+		return img;
+	}
+
+	public void setImage(String str) {
+		img = Toolkit.getDefaultToolkit().getImage("PacManImage" + type + ".png");
+	}
+	public boolean hasFood() {
+		return food;
+	}
+	public void setFood(boolean b) {
+		food = b;
+	}
+	public Image getFoodImage() {
+		return foodImg;
+	}
+
+	public void setFoodImage(String str) {
+		foodImg = Toolkit.getDefaultToolkit().getImage("PacManImage" + foodType + ".png");
+	}
+
+	public String getFoodType() {
+		return foodType;
+	}
+
+	public void setFoodType(String foodType) {
+		this.foodType = foodType;
+		setFoodImage(foodType);
+	}
+
+}
